682b4bc57ef845fa31a193c19d49212d68e775a6
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2024-10-21 10:02:14 +0100

    tailwind configs

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,31 @@ module.exports = {
 "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'blue': '#0028FF',
+      'white': '#FFFFFF',
+      'black': '#000000',
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+    },
+    fontWeight: {
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+    },
+    fontSize: {
+      t1: '18px',
+      t2: '16px',
+      h1: '56px',
+      h2: '48px',
+      h3: '40px',
+      h5: '24px',
+      h6: '20px'
+    },
     extend: {},
   },
   plugins: [],
