1a149abeed28f5df5dd2c632dc89f3252c2f5a0b
Ahmed iyanuoluwa <127423641+iyanu752@users.noreply.github.com>
2024-12-07 11:13:52 +0100

    sign up and log in routes

--- client/speed-type/src/App.jsx
+++ client/speed-type/src/App.jsx
@@ -1,5 +1,6 @@
-import './App.css'
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import './App.css';
+import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Navbar from './components/navbar';
 import Home from './screens/home';
 import Footer from './components/footer';
@@ -7,41 +8,70 @@ import About from './screens/about';
 import Analytics from './screens/analytics';
 import Challenges from './screens/challenges';
 import Leaderboard from './screens/leaderboards';
+import PropTypes from 'prop-types';
 import Contact from './screens/contact';
-function App() {
-  const router = createBrowserRouter ([
-    {
-      path: '/',
-      element: <Home/>
-    },
-    {
-      path: '/About',
-      element: <About/>
-    },
-    {
-      path: '/Analytics',
-      element: <Analytics/>
-    },
-    {
-      path: '/Challenges',
-      element: <Challenges/>
-    },
-    {
-      path: '/Leaderboard',
-      element: <Leaderboard/>
-    },
-    {
-      path: '/Contact',
-      element: <Contact/>
-    },
-  ])
+import Signup from './screens/signup';
+import Login from './screens/login';
+
+function AppLayout({ children }) {
+  const location = useLocation();
+  const [showNavbarFooter, setShowNavbarFooter] = useState(true);
+
+  useEffect(() => { 
+    const hiddenRoutes = ['/signup', '/login'];
+    setShowNavbarFooter(!hiddenRoutes.includes(location.pathname));
+  }, [location]);
+
   return (
     <>
-      <Navbar/>
-      <RouterProvider router = {router} />
-      <Footer/>
+      {showNavbarFooter && <Navbar />}
+      {children}
+      {showNavbarFooter && <Footer />}
     </>
-  )
+  );
+}
+AppLayout.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <AppLayout><Home /></AppLayout>,
+    },
+    {
+      path: '/about',
+      element: <AppLayout><About /></AppLayout>,
+    },
+    {
+      path: '/analytics',
+      element: <AppLayout><Analytics /></AppLayout>,
+    },
+    {
+      path: '/challenges',
+      element: <AppLayout><Challenges /></AppLayout>,
+    },
+    {
+      path: '/leaderboard',
+      element: <AppLayout><Leaderboard /></AppLayout>,
+    },
+    {
+      path: '/contact',
+      element: <AppLayout><Contact /></AppLayout>,
+    },
+    {
+      path: '/signup',
+      element: <div><Signup/></div>,
+    },
+    {
+      path: '/login',
+      element: <div><Login/></div>,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
